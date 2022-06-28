dabb91bfc6ef0b0e7efe6c27c7702c93e38233c3
olundel <o.shiryavceva@gmail.com>
2022-06-28 03:15:25 +0300

    add some fixes

--- lib/flutter/hw_week_9/main_hw9.dart
+++ lib/flutter/hw_week_9/main_hw9.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_homework/flutter/hw_week_9/screens/sign_in_screen.dart';
 
-void main() => runApp(const ChatApp());
+void main() => runApp(const HRApp());
 
 
-class ChatApp extends StatelessWidget {
-  const ChatApp({Key? key}) : super(key: key);
+class HRApp extends StatelessWidget {
+  const HRApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
